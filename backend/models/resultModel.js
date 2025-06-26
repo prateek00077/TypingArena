6e1cfe6d94c141e74e9f7443803b811f01b22406
prateek00077 <pandeyprateek560@gmail.com>
2025-06-26 20:27:17 +0530

    fixed result model

--- backend/models/resultModel.js
+++ backend/models/resultModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const resultSchema = new mongoose.Schema({
   userId : { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  raceId : {type: mongoose.Schema.Types.ObjectId, ref: 'Race', default: null},
+  roomId : {type: mongoose.Schema.Types.ObjectId, ref: 'Race', default: null},
   wpm: Number,
   accuracy: Number,
   charsTyped: Number,
