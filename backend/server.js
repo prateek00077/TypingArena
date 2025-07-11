d281985feedb83214576d2e2766e65cfa2222066
Abhishek Patel <itsabhishekpatel01@gmail.com>
2025-07-11 14:26:24 +0530

    [Abhishek] cors error fixed

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'http://localhost:5173',
+    credentials: true,
     methods: ['GET', 'POST'],
   },
 });
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
