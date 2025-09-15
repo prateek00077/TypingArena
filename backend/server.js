ead21a57d672cfd4604fb7f9e0a612fb00e70db6
prateek00077 <pandeyprateek560@gmail.com>
2025-09-15 10:53:54 +0530

    dynamic fontend url instead of hardcoded

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: `${process.env.FONTEND_URL}`,
     methods: ['GET', 'POST'],
     credentials: true,
   },
 });
 
 app.use(cors({
-  origin:  "http://localhost:5173",
+  origin:  `${process.env.FONTEND_URL}`,
   credentials: true,
 }));
 app.use(express.json());
